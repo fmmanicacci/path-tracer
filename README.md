9a767d1e74c9283c541565332779f5296a2a8167
François-Marie Manicacci <fm.manicacci@proton.me>
2024-09-28 18:05:30 +0200

    generate first image

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # path-tracer
+
+Run the following command to render an image and store it inside a file:
+
+```bash
+cargo run > image.ppm
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    // Image
+
+    let image_width: u32 = 256;
+    let image_height: u32 = 256;
+
+    // Render
+    
+    println!("P3\n{image_width} {image_height}\n255");
+
+    for j in 0..image_height {
+        for i in 0..image_width {
+            let r: f64 = (i as f64) / ((image_width as f64) - 1.0);
+            let g: f64 = (j as f64) / ((image_height as f64) - 1.0);
+            let b: f64 = 0.0;
+
+            let ir = (255.999 * r) as u32;
+            let ig = (255.999 * g) as u32;
+            let ib = (255.999 * b) as u32;
+
+            println!("{ir} {ig} {ib}");
+        }
+    }
 }
